23d93dfa25114c078a00512f96665e78eeead95c
elzabeatriz <50021248+elzabeatriz@users.noreply.github.com>
2019-04-26 10:49:13 -0300

    Create etapa1.js

---
+++ etapa1.js
@@ -0,0 +1,17 @@
+/*
+Equipe
+   Elza Beatriz Hermogenes de Farias
+   Etapa 1
+*/
+
+var xi = 200;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(100);
+  rect(xi, 10 ,50 ,50)
+  ellipse(200, 350, 30, 50);
+}
