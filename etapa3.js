45de127d135c1177d83ae659460ce212056faf9c
elzabeatriz <50021248+elzabeatriz@users.noreply.github.com>
2019-05-10 09:49:13 -0300

    Create etapa3.js

---
+++ etapa3.js
@@ -0,0 +1,43 @@
+/*
+Equipe
+   Elza Beatriz Hermogenes de Farias
+   Etapa 2
+*/
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
+var yo = 0;
+var xo = 50;
+var x = 200;
+var z = 350;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(100);
+  rect(xo, yo ,30 ,30);
+  yo =yo + 4   
+  if(yo > 400){
+    yo = -random(1000);
+  }
+  if (keyIsDown(LEFT_ARROW)){
+      x = x - 3
+      }
+  if (keyIsDown(RIGHT_ARROW)){
+    x = x + 3
+  }
+  if (keyIsDown(UP_ARROW)){
+     z = z - 3
+  }
+  if (keyIsDown(DOWN_ARROW)){
+      z = z + 3
+  }
+  ellipse(x, z, 30, 50);
+}
